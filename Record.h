0528a232450b04c67a79303d045b109b08e8c346
Daniel Rivas <drivas8891@gmail.com>
2024-05-13 19:15:21 -0700

    Split project into header files for better organization

---
+++ Record.h
@@ -0,0 +1,12 @@
+#ifndef RECORD_H
+#define RECORD_H
+
+/* Record information */
+struct Record {
+    char* name;
+    char* street;
+    char* city_and_state;
+    char* zipcode;
+};
+
+#endif
--- Source.c
+++ Source.c
@@ -2,104 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "Record.h"
 
 #define MAX_ADDRESS_LENGTH 50
 #define MAX_NAME_LENGTH 30
 #define MAX_RECORDS 50
 #define MAX_LINE_LENGTH 100
 
-/* Record information */
-struct Record {
-    char* name;
-    char* street;
-    char* city_and_state;
-    char* zipcode;
-};
-
-/* Helper function to remove newline character from the end of a string */
-void removeNewline(char* str) {
-    int len = strlen(str);
-    if (len > 0 && str[len - 1] == '\n') {
-        str[len - 1] = '\0';
-    }
-}
-
-void sortByZipcode(struct Record** records, int num_records) {
-    for (int i = 0; i < num_records - 1; ++i) {
-        /* Convert the current zipcode to an integer */
-        int current = records[i]->zipcode;
-        int next = records[i + 1]->zipcode;
-
-        if (current > next) {
-            struct Record* temp = records[i];
-
-            records[i] = records[i + 1];
-            records[i + 1] = temp;
-        }
-    }
-}
-
-void merge(struct Record** records, int left, int mid, int right) {
-    int i, j, k;
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
-
-    // Create temp arrays
-    struct Record** L = malloc(n1 * sizeof(struct Record*));
-    struct Record** R = malloc(n2 * sizeof(struct Record*));
-
-    // Copy data to temp arrays L[] and R[]
-    for (i = 0; i < n1; i++)
-        L[i] = records[left + i];
-    for (j = 0; j < n2; j++)
-        R[j] = records[mid + 1 + j];
-
-    // Merge the temp arrays back into records[left..right]
-    i = 0; // Initial index of first subarray
-    j = 0; // Initial index of second subarray
-    k = left; // Initial index of merged subarray
-    while (i < n1 && j < n2) {
-        if (strcmp(L[i]->zipcode, R[j]->zipcode) <= 0) {
-            records[k] = L[i];
-            i++;
-        }
-        else {
-            records[k] = R[j];
-            j++;
-        }
-        k++;
-    }
-
-    // Copy the remaining elements of L[], if there are any
-    while (i < n1) {
-        records[k] = L[i];
-        i++;
-        k++;
-    }
-
-    // Copy the remaining elements of R[], if there are any
-    while (j < n2) {
-        records[k] = R[j];
-        j++;
-        k++;
-    }
-
-    free(L);
-    free(R);
-}
-
-void mergeSort(struct Record** records, int left, int right) {
-    if (left < right) {
-        // Same as (l+r)/2, but avoids overflow for large l and h
-        int mid = left + (right - left) / 2;
-
-        // Sort first and second halves
-        mergeSort(records, left, mid);
-        mergeSort(records, mid + 1, right);
-
-        merge(records, left, mid, right);
-    }
-}
+void removeNewline(char* str); /* Helper function to remove newline character from the end of a string */
+void merge(struct Record** records, int left, int mid, int right);
+void mergeSort(struct Record** records, int left, int right);
 
 int main(void) {
     struct Record* records[MAX_RECORDS];
@@ -191,14 +103,13 @@ int main(void) {
     /* Display sorted records */
     for (int i = 0; i < num_records; i++) {
         struct Record* rec = records[i];
-        printf("Record #%d\n", i + 1);
-        printf("Name: %s\n", rec->name);
-        printf("Street: %s\n", rec->street);
-        printf("City and State: %s\n", rec->city_and_state);
-        printf("Zipcode: %s\n", rec->zipcode);
+        printf("%s\n", rec->name);
+        printf("%s\n", rec->street);
+        printf("%s\n", rec->city_and_state);
+        printf("%s\n\n", rec->zipcode);
     }
 
-    // Cleanup
+    /* Deallocate dynamically allocated memory */
     for (int i = 0; i < num_records; i++) {
         free(records[i]->name);
         free(records[i]->street);
@@ -208,4 +119,75 @@ int main(void) {
     }
 
     return 0;
+}
+
+void removeNewline(char* str) {
+    int len = strlen(str);
+    if (len > 0 && str[len - 1] == '\n') {
+        str[len - 1] = '\0';
+    }
+}
+
+void merge(struct Record** records, int left, int mid, int right) {
+    int i, j, k;
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    /* Create temporary arrays to store the two halves of the records */
+    struct Record** L = malloc(n1 * sizeof(struct Record*));
+    struct Record** R = malloc(n2 * sizeof(struct Record*));
+
+    /* Copy data to temp arrays L[] and R[] */
+    for (i = 0; i < n1; i++) {
+        L[i] = records[left + i];
+    }
+    for (j = 0; j < n2; j++) {
+        R[j] = records[mid + 1 + j];
+    }
+
+    /* Merge the temp arrays back into records[left..right] */
+    i = 0;
+    j = 0;
+    k = left;
+    while (i < n1 && j < n2) {
+        if (strcmp(L[i]->zipcode, R[j]->zipcode) <= 0) {
+            records[k] = L[i];
+            i++;
+        }
+        else {
+            records[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    /* Copy the remaining elements of L[], if there are any */
+    while (i < n1) {
+        records[k] = L[i];
+        i++;
+        k++;
+    }
+
+    /* Copy the remaining elements of R[], if there are any */
+    while (j < n2) {
+        records[k] = R[j];
+        j++;
+        k++;
+    }
+
+    free(L);
+    free(R);
+}
+
+void mergeSort(struct Record** records, int left, int right) {
+    if (left < right) {
+        /* Same as (l+r)/2, but avoids overflow for large l and h */
+        int mid = left + (right - left) / 2;
+
+        /* Sort first and second halves */
+        mergeSort(records, left, mid);
+        mergeSort(records, mid + 1, right);
+
+        merge(records, left, mid, right);
+    }
 }
